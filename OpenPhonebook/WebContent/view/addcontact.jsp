0502cd0b589f5768a05dafc54d2c475a94830734
Ivana Cupic <ivanamd82@gmail.com>
2017-05-25 17:50:48 +0200

    AddContact.java zavrsen

--- OpenPhonebook/WebContent/view/addcontact.jsp
+++ OpenPhonebook/WebContent/view/addcontact.jsp
@@ -1,5 +1,5 @@
-<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
+<%@ page language="java" contentType="text/html; charset=utf-8"
+    pageEncoding="utf-8"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -7,7 +7,7 @@
 </head>
 <body>
 	<div class="page">
-		%@ include file="navuser.jsp"%>
+		<%@ include file="navuser.jsp"%>
 		
 		<div id="content">
 			<article class="container bgimg">
--- OpenPhonebook/src/org/bildit/controller/AddContact.java
+++ OpenPhonebook/src/org/bildit/controller/AddContact.java
@@ -1,11 +1,17 @@
 package org.bildit.controller;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.bildit.model.bo.ContactBO;
+import org.bildit.model.dto.Contact;
 
 @WebServlet("/addcontact")
 public class AddContact extends HttpServlet {
@@ -19,7 +25,29 @@ public class AddContact extends HttpServlet {
 		String emailContact = request.getParameter("emailContact");
 		String phoneContact = request.getParameter("phoneContact");
 		String cityContact = request.getParameter("cityContact");
+		
+		if ( name != null && nameLContact != null) {			
+			HttpSession sesion = request.getSession();
+			String contactName = sesion.getAttribute("sessionName").toString();
+			ContactBO contactBO = new ContactBO();
 			
+			Contact contact = new Contact(name,phoneContact,contactName);
+			contact.setLastName(nameLContact);
+			contact.setEmail(emailContact);
+			contact.setCity(cityContact);
+			
+			boolean addConfirm = contactBO.addContactBO(contact);
+			
+			if (addConfirm) {
+				RequestDispatcher view = request.getRequestDispatcher("view/userhome.jsp");
+				view.forward(request, response);
+				System.out.println("Dodan");
+			}			
+		}
+		else {
+			RequestDispatcher view = request.getRequestDispatcher("view/addcontact.jsp");
+			view.forward(request, response);
+		}
 		
 }
 
